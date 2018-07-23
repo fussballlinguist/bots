73fb7a9c8218b448908672030cc1568fdd2b06a7
fussballlinguist <39825770+fussballlinguist@users.noreply.github.com>
2018-07-23 12:06:02 +0200

    Add files via upload

---
+++ zwaraber_github.pl
@@ -0,0 +1,21 @@
+open FILE, "< /Pfad/zur/Datei.txt" or die $!;
+foreach (<FILE>) {
+	my $line = $_;
+	if ($line =~ /\. ([\w ]+?zwar [\w ]+?, )/) {
+		$zwar = $1;
+		if (length($zwar) > 30) {
+			$zwar_hash{$zwar}++;
+		}
+	}
+	if ($line =~ /Aber ([\w ]{50,100}\.)/) {
+		$aber = $1;
+		$aber_hash{$aber}++;
+	}
+}
+for (my $i = 0; $i < 1000; $i++) {
+	@zwar_keys = keys %zwar_hash;
+	$zwar_token = $zwar_keys[int rand(@zwar_keys)];
+	@aber_keys = keys %aber_hash;
+	$aber_token = $aber_keys[int rand(@aber_keys)];
+	print "$zwar_token" . "aber " . "$aber_token\n";
+}
